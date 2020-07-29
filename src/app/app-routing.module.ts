51ad76a2e08c30eef0e0146ee117cb36e39bb59f
Ricardo San Martín <rsmh88@gmail.com>
2020-07-28 22:37:57 -0500

    Fin seccion 4 - Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
 
-import {LoginComponent} from './auth/login/login.component';
-import {RegisterComponent} from './auth/register/register.component';
 import {NopagefoundComponent} from './nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+
 
 const routes: Routes =[
-  {path: 'register', component: RegisterComponent },
-  {path: 'login', component: LoginComponent },
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: '**', component: NopagefoundComponent },
 ];
 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes),
-  PagesRoutingModule],
+  PagesRoutingModule,
+  AuthRoutingModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 
+
 @NgModule({
   declarations: [
     LoginComponent,
